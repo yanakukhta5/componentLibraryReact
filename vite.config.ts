49ca529327a40a3c1481287a30c3120027205c7e
Yana Kukhta <kukhta0school@gmail.com>
2023-03-13 15:27:06 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    lib: {
+      name: 'KubUI',
+      formats: ['es', 'cjs', 'umd'],
+      entry: path.resolve(__dirname, 'src', 'index.ts')
+    },
+    rollupOptions: {
+      external: ['react', 'ReactDOM'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
