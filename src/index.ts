e1a1e79cf4d300088048e456753e0ce27bc1f415
Yana Kukhta <kukhta0school@gmail.com>
2023-03-16 01:44:03 +0300

    add some components to index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { Logotype } from '@/components/Logotype'
 import { KubThemeProvider } from '@/KubThemeProvider'
+import { Button } from '@/components/Button'
+import { Image } from '@/components/Image'
+import { Paragraph } from '@/components/Paragraph'
 
-export { Logotype, KubThemeProvider }
+export { Logotype, KubThemeProvider, Button, Image, Paragraph }
