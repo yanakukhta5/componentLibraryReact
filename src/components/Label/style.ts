72ab8c7102648bb3c12caeb0dc62fddcef868ae0
Yana Kukhta <kukhta0school@gmail.com>
2023-04-20 23:58:09 +0300

    fix Label

--- src/components/Label/style.ts
+++ src/components/Label/style.ts
@@ -1,3 +1,6 @@
 import styled from '@emotion/styled'
 
-export const Wrapper = styled.label``
+export const Wrapper = styled.label`
+  user-select: none;
+  font-family: ${({theme}) => theme.fonts.primary};
+`
