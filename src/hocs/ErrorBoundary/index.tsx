b496ed059df17385cf3aedfb3fc16aac53f423f3
Yana Kukhta <kukhta0school@gmail.com>
2023-04-30 14:36:03 +0300

    add ErrorBoundary hoc

---
+++ src/hocs/ErrorBoundary/index.tsx
@@ -0,0 +1,29 @@
+import { Component, ErrorInfo } from 'react'
+
+import { ErrorBoundaryProps, ErrorBoundaryState } from './types'
+
+export class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error(error, errorInfo)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback
+    }
+
+    return this.props.children
+  }
+}
