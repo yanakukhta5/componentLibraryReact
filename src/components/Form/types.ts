f0a8e91e6f4ccc93eb65a698e106b460911c138f
Yana Kukhta <kukhta0school@gmail.com>
2023-05-01 16:10:54 +0300

    add method and action to form

--- src/components/Form/types.ts
+++ src/components/Form/types.ts
@@ -4,4 +4,6 @@ import { HTMLAttributes } from "react"
 export type FormProps = HTMLAttributes<HTMLFormElement> & Partial<{
  borderColor: TColor
  width: number
+ method: string
+ action: string
 }>
